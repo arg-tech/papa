0f124d4870af62b9d8305f9b68f706b2e0a7c5c8
Joshua Price <git@jprice.uk>
2025-10-03 19:20:17 +0100

    Return traceback when all_analytics fails
    Although this may be unusual to do in a more production-like setting, it
    may be very helpful to people trying to call this.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import traceback
+
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, ConfigDict
 from . import papa
 
@@ -17,7 +19,12 @@ app = FastAPI()
 # Call without async so that fastapi does the work on a threadpool as
 # all_analytics is cpu-bound and we don't want to block the current thread.
 def all_analytics(body: RequestBody | dict):
-    if isinstance(body, RequestBody):
-        return papa.all_analytics(body.xaif, **{k: v for k, v in body if k != "xaif"})
-    else:
-        return papa.all_analytics(body)
+    try:
+        if isinstance(body, RequestBody):
+            return papa.all_analytics(
+                body.xaif, **{k: v for k, v in body if k != "xaif"}
+            )
+        else:
+            return papa.all_analytics(body)
+    except Exception:
+        raise HTTPException(status_code=500, detail=traceback.format_exc())
